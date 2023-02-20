064f73b1175ddefd8c69718aff77a8bc0be0d321
Phan Vuong <pdvongtb@gmail.com>
2023-02-20 23:17:45 +0700

    Articles Page

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -2,13 +2,15 @@
 
 import React from 'react';
 import MainLayout from '../components/Layout/MainLayout';
+import Articles from './Articles';
 import Hero from './Hero';
 
 const HomePage = () => {
   return (
     <>
       <MainLayout>
-        <Hero/>
+        <Hero />
+        <Articles/>
       </MainLayout>
     </>
   );
